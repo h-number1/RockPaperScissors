a4e0a4c0f3c3545757692954bf5f21fb1efe45c6
h1 <187214660+h-number1@users.noreply.github.com>
2024-12-17 20:10:34 +0000

    Feat: Write function for computer choice
    The computer is needing to return a random choice
    of the three options within the game when this
    function is called. At this stage I've set up
    the function name and created variables for the
    three string values that the game will utilise.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+
+
+function getComputerChoice() {
+
+}
+
+//Create a function that represents the computer choice
+//This function will randomly return a value of rock, paper or scissors.
